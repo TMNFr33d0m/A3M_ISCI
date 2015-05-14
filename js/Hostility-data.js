09c3c04ad1c438d93c1bb915bf4bd0e761b0f40e
TMNFr33d0m <csalazar@revolusys.com>
2015-05-13 17:23:15 -0700

    Initial commit Pt 2

---
+++ js/Hostility-data.js
@@ -0,0 +1,354 @@
+var gdpData = {
+//  Bangladesh
+"BD":0,
+//  Belgium
+"BE":0,
+//  Burkina Faso
+"BF":0,
+//  Bulgaria
+"BG":0,
+// 	Bosnia and Herz.
+"BA":0,
+// 	Brunei
+"BN":0,
+// 	Bolivia
+"BO":0,
+//  Japan
+"JP":20,
+//  Burundi
+"BI":0,
+// 	Benin
+"BJ":0,
+// 	Bhutan
+"BT":0,
+// 	Jamaica
+"JM":0,
+// 	Botswana
+"BW":0,
+// 	Brazil
+"BR":0,
+// 	Bahamas
+"BS":0,
+//  Belarus
+"BY":0,
+// 	Belize
+"BZ":0,
+// 	Russia
+"RU":80,
+// 	Rwanda
+"RW":0,
+// 	Serbia
+"RS":0,
+// 	Lithuania
+"LT":0,
+// 	Luxembourg
+"LU":0,
+// 	Liberia
+"LR":0,
+// 	Romania
+"RO":0,
+// 	Guinea-Bissau
+"GW":0,
+// 	Guatemala
+"GT":0,
+// 	Greece
+"GR":0,
+//  Eq. Guinea
+"GQ":0,
+//  Guyana
+"GY":0,
+//  Georgia
+"GE":0,
+//  United Kingdom
+"GB":0,
+//  Gabon
+"GA":0,
+//  Guinea
+"GN":0,
+//  Gambia
+"GM":0,
+// 	Greenland
+"GL":0,
+// 	Kuwait
+"KW":0,
+// 	Ghana
+"GH":0,
+// 	Oman
+"OM":0,
+// Somaliland
+"_1":0,
+// Kosovo
+"_0":0	,
+// 	Jordan
+"JO":0,
+// 	Croatia
+"HR":0,
+// 	Haiti
+"HT":0,
+// 	Hungary
+"HU":0,
+// 	Honduras
+"HN":0,
+// 	Puerto Rico
+"PR":0,
+// 	Palestine
+"PS":0,
+// 	Portugal
+"PT":0,
+// 	Paraguay
+"PY":0,
+// 	Panama
+"PA":0,
+// 	Papua New Guinea
+"PG":0,
+// 	Peru
+"PE":0,
+// 	Pakistan
+"PK":0,
+//  Philippines
+"PH":0,
+//  Poland
+"PL":0,
+// 	N. Cyprus
+"-99":0,
+//  Zambia
+"ZM":0,
+// 	W. Sahara
+"EH":0,
+// 	Estonia
+"EE":0,
+// 	Egypt
+"EG":80,
+// 	South Africa
+"ZA":0,
+// 	Ecuador
+"EC":0,
+// 	Albania
+"AL":0,
+// 	Angola
+"AO":0,
+// 	Kazakhstan
+"KZ":0,
+// 	Ethiopia
+"ET":0,
+// 	Zimbabwe
+"ZW":0,
+//  Spain
+"ES":0,
+//  Eritrea
+"ER":0,
+// 	Montenegro
+"ME":0,
+// 	Moldova
+"MD":0,
+//  Madagascar
+"MG":0,
+// 	Morocco
+"MA":0,
+// 	Uzbekistan
+"UZ":0,
+// 	Myanmar
+"MM":0,
+// 	Mali
+"ML":80,
+// 	Mongolia
+"MN":0,
+// 	Macedonia
+"MK":0,
+// 	Malawi
+"MW":0,
+// 	Mauritania
+"MR":0,
+// 	Uganda
+"UG":0,
+//  Malaysia
+"MY":0,
+//  Mexico
+"MX":0,
+// 	Vanuatu
+"VU":0,
+// 	France
+"FR":0,
+// 	Finland
+"FI":0,
+// 	Fiji
+"FJ":0,
+// 	Falkland Is.
+"FK":0,
+// 	Nicaragua
+"NI":0,
+// 	Netherlands
+"NL":0,
+// 	Norway
+"NO":0,
+// 	Namibia
+"NA":0,
+//  New Caledonia
+"NC":0,
+// 	Niger
+"NE":80,
+// 	Nigeria
+"NG":70,
+//	New Zealand
+"NZ":0,
+// 	Nepal
+"NP":0,
+// Côte d'Ivoire
+"CI":0,
+// 	Switzerland
+"CH":0,
+// 	Colombia
+"CO":0,
+//  China
+"CN":20,
+//  Cameroon
+"CM":0,
+// Chile
+"CL":0,
+// Canada
+"CA":0,
+// Congo
+"CG":0,
+// Central African Rep.
+"CF":0,
+// Dem. Rep. Congo
+"CD":0,
+// 	Czech Rep.
+"CZ":0,
+// 	Cyprus
+"CY":0,
+// 	Costa Rica
+"CR":0,
+//  Cuba
+"CU":0,
+// 	Swaziland
+"SZ":0,
+//  Syria
+"SY":90,
+// 	Kyrgyzstan
+"KG":0,
+// 	Kenya
+"KE":0,
+// 	S. Sudan
+"SS":0,
+//  Suriname
+"SR":0,
+// 	Cambodia
+"KH":0,
+// 	El Salvador
+"SV":0,
+//  Slovakia
+"SK":0,
+//  Korea
+"KR":0,
+// 	Slovenia
+"SI":0,
+// 	Dem. Rep. Korea
+"KP":100,
+// 	Somalia
+"SO":0,
+//  Senegal
+"SN":0,
+// 	Sierra Leone
+"SL":0,
+//  Solomon Is.
+"SB":0,
+// 	Saudi Arabia
+"SA":0,
+// 	Sweden
+"SE":0,
+// 	Sudan
+"SD":0,
+//  Dominican Rep.
+"DO":0,
+// 	Djibouti
+"DJ":0,
+//  Denmark
+"DK":0,
+// 	Germany
+"DE":0,
+// 	Yemen
+"YE":0,
+//  Austria
+"AT":0,
+// 	Algeria
+"DZ":0,
+// 	United States
+"US":0,
+//  Latvia
+"LV":0,
+//  Uruguay
+"UY":0,
+// 	Lebanon
+"LB":40,
+// 	Lao PDR
+"LA":0,
+//  Taiwan
+"TW":0,
+// 	Trinidad and Tobago
+"TT":0,
+// 	Turkey
+"TR":80,
+//  Sri Lanka
+"LK":0,
+// 	Tunisia
+"TN":0,
+// 	Timor-Leste
+"TL":0,
+//  Turkmenistan
+"TM":0,
+//  Tajikistan
+"TJ":0,
+//  Lesotho
+"LS":0,
+// 	Thailand
+"TH":60,
+// 	Fr. S. Antarctic Lands
+"TF":0,
+// 	Togo
+"TG":0,
+// 	Chad
+"TD":80,
+// 	Libya
+"LY":80,
+// 	United Arab Emirates
+"AE":0,
+//  Venezuela
+"VE":0,
+// 	Afghanistan
+"AF":100,
+// 	Iraq
+"IQ":100,
+// 	Iceland
+"IS":0,
+// 	Iran
+"IR":100,
+// 	Armenia
+"AM":0,
+// 	Italy
+"IT":0,
+//  Vietnam
+"VN":40,
+// 	Argentina
+"AR":0,
+// 	Australia
+"AU":0,
+// 	Israel
+"IL":0,
+// 	India
+"IN":0,
+// 	Tanzania
+"TZ":0,
+//  Azerbaijan
+"AZ":0,
+// 	Ireland
+"IE":0,
+//  Indonesia
+"ID":30,
+// 	Ukraine
+"UA":80,
+//  Qatar
+"QA":0,
+// 	Mozambique
+"MZ":0
+}; 
---
+++ js/_vti_cnf/Hostility-data.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|30 Apr 2015 22:09:04 -0000
+vti_syncwith_localhost\\c\:\\users\\cody\\documents\\my web sites\\2abct/c\:/users/cody/documents/my web sites/2abct:TR|24 Feb 2015 23:24:57 -0000
+vti_author:SR|SIERRABRAVONOC\\Cody
+vti_modifiedby:SR|SIERRABRAVONOC\\Cody
+vti_nexttolasttimemodified:TW|30 Apr 2015 22:06:37 -0000
+vti_timecreated:TR|14 Mar 2015 02:16:46 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_syncwith_localhost\\\\\\192.168.0.8\\inetpub\\wwwroot\\327///192.168.0.8/inetpub/wwwroot/327:TR|30 Apr 2015 22:09:04 -0000
+vti_backlinkinfo:VX|leader.html
+vti_cacheddtm:TX|30 Apr 2015 22:06:37 -0000
+vti_filesize:IR|4095
